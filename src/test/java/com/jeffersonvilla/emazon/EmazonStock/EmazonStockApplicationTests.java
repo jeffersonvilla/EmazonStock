c9c6ac5091728d42e6661ea275bbf62fc9b8ec69
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2024-08-15 14:26:38 -0500

    Inicio del proyecto

---
+++ src/test/java/com/jeffersonvilla/emazon/EmazonStock/EmazonStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffersonvilla.emazon.EmazonStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmazonStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
