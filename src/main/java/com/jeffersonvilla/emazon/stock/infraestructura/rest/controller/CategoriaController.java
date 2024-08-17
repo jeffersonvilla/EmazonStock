f74e2f78a060f7e8a40b537c27e84df6a9d47fdb
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2024-08-17 14:06:05 -0500

    implementa documentación con swagger

--- src/main/java/com/jeffersonvilla/emazon/stock/infraestructura/rest/controller/CategoriaController.java
+++ src/main/java/com/jeffersonvilla/emazon/stock/infraestructura/rest/controller/CategoriaController.java
@@ -4,6 +4,12 @@ import com.jeffersonvilla.emazon.stock.dominio.api.ICategoriaServicePort;
 import com.jeffersonvilla.emazon.stock.infraestructura.rest.dto.CrearCategoriaRequestDto;
 import com.jeffersonvilla.emazon.stock.infraestructura.rest.dto.CrearCategoriaResponseDto;
 import com.jeffersonvilla.emazon.stock.infraestructura.rest.mapper.CategoriaMapperRest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Categoria API", description = "Operaciones relacionadas con las categorías")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("categoria")
@@ -21,6 +28,17 @@ public class CategoriaController {
     private final ICategoriaServicePort crearCategoriaApi;
     private final CategoriaMapperRest mapper;
 
+    @Operation(summary = "Crear nueva categoría",
+            description = "Crea una nueva categoría con los datos suministrados en el body.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Categoría creada exitosamente",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CrearCategoriaResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(mediaType = "application/json"))
+    })
     @PostMapping("crear")
     public ResponseEntity<CrearCategoriaResponseDto> crearCategoria(
             @Valid
