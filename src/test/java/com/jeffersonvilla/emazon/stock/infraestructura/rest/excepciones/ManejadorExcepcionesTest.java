3ba7f636eb1812facf2e6fe1f657d2497980afe8
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2024-08-21 20:00:48 -0500

    agrego validación para los dto de historia de usuario 1 (hu1), como se indica en reunion de avances, además de test unitarios para el manejador de excepciones que recibe la validación del dto

--- src/test/java/com/jeffersonvilla/emazon/stock/infraestructura/rest/excepciones/ManejadorExcepcionesTest.java
+++ src/test/java/com/jeffersonvilla/emazon/stock/infraestructura/rest/excepciones/ManejadorExcepcionesTest.java
@@ -6,8 +6,18 @@ import com.jeffersonvilla.emazon.stock.dominio.excepciones.NombreNoValidoExcepti
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ManejadorExcepcionesTest {
 
@@ -56,5 +66,35 @@ class ManejadorExcepcionesTest {
         assertNotNull(response.getBody());
         assertEquals(HttpStatus.BAD_REQUEST.toString(), response.getBody().status());
         assertEquals(mensajeError, response.getBody().message());
+    }
+
+    @Test
+    void testHandleMethodArgumentNotValidException() {
+
+        ObjectError error1 = new FieldError("nombreObjeto", "campo1", "mensaje error 1");
+        ObjectError error2 = new FieldError("nombreObjeto", "campo2", "mensaje error 2");
+
+        MethodArgumentNotValidException methodArgumentNotValidException =
+                mock(MethodArgumentNotValidException.class);
+
+        BindingResult bindingResult = mock(BindingResult.class);
+
+        List<ObjectError> listaErrores = Arrays.asList(error1, error2);
+        when(methodArgumentNotValidException.getBindingResult()).thenReturn(bindingResult);
+        when(bindingResult.getAllErrors()).thenReturn(listaErrores);
+
+
+        ResponseEntity<RespuestaConVariosErrores> response =
+                manejadorExcepciones.handleMethodArgumentNotValidException(methodArgumentNotValidException);
+
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        RespuestaConVariosErrores body = response.getBody();
+        assertEquals(HttpStatus.BAD_REQUEST.toString(), body.status());
+
+        Map<String, String> errores = body.mensajes();
+        assertEquals(2, errores.size());
+        assertEquals("mensaje error 1", errores.get("campo1"));
+        assertEquals("mensaje error 2", errores.get("campo2"));
     }
 }
