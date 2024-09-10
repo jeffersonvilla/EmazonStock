513b5d51821c30131c42972cc709f0a81aade438
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2024-09-10 10:31:04 -0500

    feat(hu8): agrega seguridad con spring security y jwt

---
+++ src/main/java/com/jeffersonvilla/emazon/stock/configuracion/SeguridadConfig.java
@@ -0,0 +1,52 @@
+package com.jeffersonvilla.emazon.stock.configuracion;
+
+import com.jeffersonvilla.emazon.stock.infraestructura.seguridad.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static com.jeffersonvilla.emazon.stock.dominio.util.Constantes.ROL_ADMIN;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SeguridadConfig {
+
+    private final JwtAuthenticationFilter jwtFilter;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/articulo/listar", "/categoria/listar", "/marca/listar")
+                        .permitAll()
+                        .requestMatchers(
+                                "/articulo/crear", "/categoria/crear", "/marca/crear")
+                        .hasAuthority(ROL_ADMIN)
+                        .anyRequest().permitAll()
+                )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+            throws Exception {
+
+        return config.getAuthenticationManager();
+    }
+
+}
---
+++ src/main/java/com/jeffersonvilla/emazon/stock/infraestructura/seguridad/JwtAuthenticationFilter.java
@@ -0,0 +1,95 @@
+package com.jeffersonvilla.emazon.stock.infraestructura.seguridad;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jeffersonvilla.emazon.stock.infraestructura.rest.excepciones.RespuestaError;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String BEARER = "Bearer ";
+    private static final Integer TAMANO_HEADER = 7;
+
+    private static final String JWT_TOKEN_EXPIRADO = "El token jwt ha expirado";
+    private static final String JWT_TOKEN_NO_VALIDO = "El token jwt no es válido";
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader(AUTHORIZATION);
+        final String jwt;
+        if (authHeader == null || !authHeader.startsWith(BEARER)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(TAMANO_HEADER);
+
+        try {
+
+            String username = jwtService.extraerUsername(jwt);
+            String rol = jwtService.extraerRol(jwt);
+
+            if (
+                    username != null && rol != null &&
+                    SecurityContextHolder.getContext().getAuthentication() == null &&
+                    jwtService.tokenValido(jwt)
+            ) {
+
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(
+                                username, null,
+                                List.of(new SimpleGrantedAuthority(rol)));
+
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+
+            filterChain.doFilter(request, response);
+
+        } catch (ExpiredJwtException e) {
+            enviarRespuestaError(response, HttpStatus.UNAUTHORIZED.value(), JWT_TOKEN_EXPIRADO);
+        } catch (SignatureException | MalformedJwtException | IllegalArgumentException e) {
+            enviarRespuestaError(response, HttpStatus.UNAUTHORIZED.value(), JWT_TOKEN_NO_VALIDO);
+        }
+    }
+
+    private void enviarRespuestaError(
+            HttpServletResponse response, int status, String message
+    ) throws IOException {
+
+        response.setStatus(status);
+        response.setContentType(MediaType.APPLICATION_JSON.toString());
+        response.getWriter().write(new ObjectMapper().writeValueAsString(
+                new RespuestaError(HttpStatus.UNAUTHORIZED.toString(), message)));
+        response.getWriter().flush();
+    }
+
+}
